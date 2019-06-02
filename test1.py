de34e404255677eba5dcc3dff6cad6a88e9ada3e
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-06-03 01:40:44 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,13 @@
+
+
+def ali():
+ list=["ali","max","ali","max","max"]
+ print (list)
+
+ a=input("--------> ")
+ print (" ")
+ print("nampur ==>",list.count(a))
+#print("max 2==>",list.count(a))
+
+ ali()
+ali()
