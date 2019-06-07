d3da6b5e643e08179373936413c1a002d301b020
Ali898989 <42411309+Ali898989@users.noreply.github.com>
2019-06-07 18:17:25 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,25 @@
+
+
+def ali():
+ list=["ali","max","ali","max","max"]
+ print (list)
+
+ a=input("--------> ")
+ print (" ")
+ print("nampur ==>",list.count(a))
+
+#print("max 2==>",list.count(a))
+
+ aa()
+
+
+def aa():
+  a=input("ali ===> ")
+  if a in "2":
+   print ("good")
+  else :
+   print ("no")
+  aa()
+
+
+ali()
